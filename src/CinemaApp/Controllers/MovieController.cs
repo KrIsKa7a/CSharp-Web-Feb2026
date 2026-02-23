8ae6deecc928af818589b9b1a38267e04e6ce4ed
Kristiyan Ivanov <kristiqn.ivanov@abv.bg>
2026-02-23 21:25:39 +0200

    Implementation of Movies/Index page functionality.

---
+++ src/CinemaApp/Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿namespace CinemaApp.Web.Controllers
+{
+    using Services.Core.Contracts;
+    using ViewModels.Movie;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class MovieController : BaseController
+    {
+        private readonly IMovieService movieService;
+
+        public MovieController(IMovieService movieService)
+        {
+            this.movieService = movieService;
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<AllMoviesIndexViewModel> allMoviesViewModel = await movieService
+                .GetAllMoviesOrderedByTitleAsync();
+
+            return View(allMoviesViewModel);
+        }
+    }
+}
